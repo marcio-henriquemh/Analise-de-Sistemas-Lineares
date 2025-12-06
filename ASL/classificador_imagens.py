f074d7ff74a07f81fc5990ee278598bc49860f3b
Márcio Henrique Matos de Freitas <70753463+marcio-henriquemh@users.noreply.github.com>
2025-12-06 20:27:15 -0300

    Add files via upload

--- ASL/classificador_imagens.py
+++ ASL/classificador_imagens.py
@@ -8,7 +8,7 @@ def funcao_degrau(z):
 
    return np.where(z >= 0, 1, 0)
 
-imagem=Image.open('/home/marciohenrique/UFS/ASL/bethoven.jpeg')
+imagem=Image.open('/home/marciohenrique/UFS/Analise-de-Sistemas-Lineares/bethoven.jpeg')
 imagem.show()
 
 print(f"formato{imagem.format}")
@@ -17,27 +17,18 @@ print(f"modo:{imagem.mode}")
 
 #converter em tons de cinza
 
-imagem=Image.open('/home/marciohenrique/UFS/ASL/bethoven.jpeg').convert('L')
+imagem=Image.open('/home/marciohenrique/UFS/Analise-de-Sistemas-Lineares/bethoven.jpeg').convert('L')
 img_array = np.array(imagem, dtype=float)
 print("Array da imagem",img_array)
 
-
 #normalizar a imagem para 0 e 1 que será a nossa entrada
 
-imagem_normalizada=img_array/255
+imagem_normalizada=((img_array/255)-0.5)
 print("Array da imagem normalizada(entrada)",imagem_normalizada)
 
 
-#pesos e bias
-
-peso=17.0
-bias=-3.0
-
-
-#aplicando na funcao
-z=imagem_normalizada*peso+bias
+z=imagem_normalizada
 saida= funcao_degrau(z)
-
 
 # Exibir resultados
 plt.figure(figsize=(10,4))
@@ -47,10 +38,6 @@ plt.title("Imagem Original")
 plt.imshow(imagem_normalizada, cmap='gray')
 plt.axis('off')
 
-plt.subplot(1,3,2)
-plt.title("Soma: w·x + b")
-plt.imshow(z, cmap='gray')
-plt.axis('off')
 
 plt.subplot(1,3,3)
 plt.title("Saída (Função Degrau)")
